e98d64e6b14d66537ed2fa1f4a3f856170fc59ac
Bartek Jaszczak <bartlomiej.jaszczak@proton.me>
2024-11-20 19:49:28 +0100

    fix: consistent macro highlights

--- lua/finale/highlights/syntax.lua
+++ lua/finale/highlights/syntax.lua
@@ -158,7 +158,7 @@ function M.get_highlights(theme)
 
         ["@constant"] = { fg = theme.syntax.constant },
         ["@constant.builtin"] = { link = "@constant", bold = true },
-        -- ["@constant.macro"] = {},
+        ["@constant.macro"] = { theme.syntax.preproc },
 
         ["@module"] = { fg = theme.syntax.module },
         ["@module.builtin"] = { fg = theme.syntax.module, bold = true },
